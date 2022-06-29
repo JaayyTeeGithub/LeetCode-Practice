3f6f899db2b18ff4d88478c22b5bfeaa6e1018f1
Jeremy Thomas <108291743+JaayyTeeGithub@users.noreply.github.com>
2022-06-29 18:23:34 -0500

    Time: 34 ms (98.95%), Space: 14.2 MB (44.15%) - LeetHub

---
+++ 205-isomorphic-strings/205-isomorphic-strings.py
@@ -0,0 +1,27 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        # map letters to each other in dictionary
+        map = {}
+
+        # if the lengths do not match it is false
+        if len(s) != len(t):
+            print(False)
+
+        for index in range(0, len(s)):
+            
+            # if the key from s and value from t is not found in map add them
+            if s[index] not in map:
+                if t[index] not in map.values():
+                    map[s[index]] = t[index]
+                # if s key is found and t value is not found then it is false
+                else:
+                    return False
+                
+            # if s key is found but does not match the current index of t it is false
+            elif map[s[index]] != t[index]:
+                return False
+            
+        # if all values have been added without finding a mismatch it is true
+        return True
+
+    
