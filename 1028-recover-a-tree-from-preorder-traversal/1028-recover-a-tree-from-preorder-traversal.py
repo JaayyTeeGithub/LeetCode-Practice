4f6311d445cb445a6298d5b96c423ad6c296c2c3
Jeremy Thomas <108291743+JaayyTeeGithub@users.noreply.github.com>
2022-07-02 18:22:49 -0500

    Time: 412 ms (5.23%), Space: 14.9 MB (17.32%) - LeetHub

---
+++ 1028-recover-a-tree-from-preorder-traversal/1028-recover-a-tree-from-preorder-traversal.py
@@ -0,0 +1,66 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def recoverFromPreorder(self, traversal: str) -> Optional[TreeNode]:
+        head_node = TreeNode()
+        depth = 0
+        current_node = None
+        num_string = ''
+        index = 0
+            
+        while index in range(0, len(traversal)):
+            if index == 0:
+                index, num_string = getNumber(traversal, index)
+                head_node.val = int(num_string)
+                print('created head')
+            elif traversal[index] == '-':
+                depth += 1
+                index += 1
+            else:
+                current_node = head_node
+                index, num_string = getNumber(traversal, index)
+                print('number is')
+                print(num_string)
+                    
+                while depth > 0:
+                    
+                    # if right of current node is none go left
+                    if current_node.right is not None:
+                        temp_node = current_node.right
+                        previous_node = current_node
+                        current_node = temp_node
+                        depth -= 1
+                        print('went right')
+                        
+                    # if not go left
+                    else:
+                        temp_node = current_node.left
+                        previous_node = current_node
+                        current_node = temp_node
+                        depth -= 1
+                        print('went left')
+                print('node at depth is')
+                print(previous_node)
+                # once at depth if left is none make left
+                if previous_node.left:
+                    previous_node.right= TreeNode(int(num_string))
+
+                # else make right
+                else:
+                    previous_node.left = TreeNode(int(num_string))
+
+                num_string = ''
+                        
+        return head_node
+    
+def getNumber(traversal, index):
+    num_string = ''
+    while index < len(traversal) and traversal[index].isdigit():
+        num_string += traversal[index]
+        index += 1
+    return index, num_string
+                
